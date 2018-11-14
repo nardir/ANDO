395d291c4091790049ee0557262aa866c10301e1
Nardi Rens <nardir@axerrio.com>
2018-11-14 16:29:29 +0100

    Initial Commit

---
+++ ANDO.CA/ANDO.CA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ANDO.CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From ANDO!");
+        }
+    }
+}
